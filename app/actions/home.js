bb6078ce9ab0471fb264b6bbdd19f4db68105a26
Mosima <mosimagodfrey@gmail.com>
2021-05-25 19:23:04 +0200

    CJANGES

--- app/actions/home.js
+++ app/actions/home.js
@@ -39,9 +39,11 @@ export const fetchCartTotal = () => {
   return (dispacth, getState) => {
     let totPrice = getState().home.cart.length > 0?getState().home.cart.reduce((a, b) => +a + +b.newPrice, 0):0
     console.log(totPrice);
+    console.log(totPrice);
+    console('Total price',getState().home.cart.length === 1 ? getState().home.cart[0].price : totPrice)
     dispacth({
       type: homeTypes.MODIFY_CART_TOTAL_PRICE,
-      totPrice: getState().home.cart.length === 1 ? getState().home.cart[0].price : totPrice
+      totPrice: totPrice === NaN ? getState().home.cart[0].price : totPrice
     })
 
   };
